bbd669cb27f2db52058a88ff9996241d74305269
Defeater23 <thedefeater834@gmail.com>
2025-08-31 22:58:57 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,34 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import ConsumerPortal from "./pages/ConsumerPortal";
+import IndustryPortal from "./pages/IndustryPortal";
+import FarmerPortal from "./pages/FarmerPortal";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login/:role" element={<Login />} />
+          <Route path="/consumer" element={<ConsumerPortal />} />
+          <Route path="/industry" element={<IndustryPortal />} />
+          <Route path="/farmer" element={<FarmerPortal />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
